ec815c2713b4a8f5537e8b215bdbd68213a7b104
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-12-11 13:07:42 -0300

    ESLint config

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
-  response.send('Hello World!')
-})
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
 
-app.listen(3000, ()=>console.log('server started at localhost:3000'));
+app.listen(3000, () => console.log('server started at localhost:3000'));
