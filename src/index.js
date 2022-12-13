2d05faf910542376fb8bde82fa6059d746082fe0
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-12-13 17:56:11 -0300

    create contact controller and rotes file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
-const app = express();
+const routes = require('./routes');
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
+const app = express();
+app.use(routes);
 
 app.listen(3000, () => console.log('server started at localhost:3000'));
