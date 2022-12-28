8432714ce2c27a56063b09852ec89dca2ef7c4c8
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-12-28 15:52:25 -0300

    creating newContacts route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => console.log('server started at localhost:3000'));
